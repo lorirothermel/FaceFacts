6dd47e46cdebc10a3dbeddf190f06720a8da92ae
Lori Rothermel <lori@rothermel1.com>
2024-12-03 12:26:50 -0800

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Lori Rothermel on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
